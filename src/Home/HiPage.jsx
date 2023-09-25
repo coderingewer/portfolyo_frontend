d5a914c69a6f118419731a18419a8da03f73a625
coderingewer <yatli5645@gmail.com>
2023-09-25 14:40:31 +0300

    portfolyo web site

---
+++ src/Home/HiPage.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "./HiPage.css";
+import "../fonts/font.css"
+
+function HiPage() {
+  return (
+    <div className="hipage" >
+      <div className="hellocontent">
+        <h1>HI,
+            I'M CARDINALPANDA
+            FULL STACK DEVELOPER</h1>
+      </div>
+    </div>
+  );
+}
+
+export default HiPage;
